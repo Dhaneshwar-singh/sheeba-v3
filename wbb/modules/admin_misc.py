1ec6b36e1fb0efb272bd1e7557d245ae05dbaabd
DN-ST <50408365+DN-ST@users.noreply.github.com>
2025-10-01 16:14:49 +0530

    Update admin_misc.py

--- wbb/modules/admin_misc.py
+++ wbb/modules/admin_misc.py
@@ -53,33 +53,6 @@ async def set_chat_title(_, message):
     )
 
 
-@app.on_message(
-    filters.command("set_user_title")
-    & ~filters.private
-    & ~filters.edited
-)
-@adminsOnly("can_change_info")
-async def set_user_title(_, message):
-    if not message.reply_to_message:
-        return await message.reply_text(
-            "Reply to user's message to set his admin title"
-        )
-    if not message.reply_to_message.from_user:
-        return await message.reply_text(
-            "I can't change admin title of an unknown entity"
-        )
-    chat_id = message.chat.id
-    from_user = message.reply_to_message.from_user
-    if len(message.command) < 2:
-        return await message.reply_text(
-            "**Usage:**\n/set_user_title NEW ADMINISTRATOR TITLE"
-        )
-    title = message.text.split(None, 1)[1]
-    await app.set_administrator_title(chat_id, from_user.id, title)
-    await message.reply_text(
-        f"Successfully Changed {from_user.mention}'s Admin Title To {title}"
-    )
-
 
 @app.on_message(
     filters.command("set_chat_photo")
